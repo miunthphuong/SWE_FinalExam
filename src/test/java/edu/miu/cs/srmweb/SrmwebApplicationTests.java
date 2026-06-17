0cfe70252768a1cf63d91e187d922026884e9262
Phuong Nguyen <miu.nthphuong@gmail.com>
2026-06-17 10:26:25 -0500

    first commit

---
+++ src/test/java/edu/miu/cs/srmweb/SrmwebApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.srmweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrmwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
